e590f42fdf0a0f64d995fb6356fcc8f47347c061
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-04-12 23:45:07 -0700

    more progress on day 1 project, reworked html and app.js

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 var portfolioItems = [];
 
 function PortfolioItem (opts) {
-  this.projectTitle = opts.projectTitle;
-  this.projectCategory = opts.projectCategory;
-  this.projectRepositoryUrl = opts.projectRepositoryUrl;
-  this.projectCompletedOn = opts.projectCompletedOn;
-  this.projectReadMe = opts.projectReadMe;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.repositoryUrl = opts.repositoryUrl;
+  this.date = opts.date;
+  this.readMe = opts.readMe;
 }
 
 Portfolio.prototype.toHtml = function() {
   var $newPortfolioItem = $('portfolioItem.template').clone();
 
-  $newPortfolioItem.attr('data-category', this.category);
-  $newPortfolioItem.find('a').html(this.author);
-  $newPortfolioItem.find('a').attr('href', this.authorUrl);
+//Code Below Needs Reworking
   $newPortfolioItem.find('h1').html(this.title);
-  $newPortfolioItem.find('.article-body').html(this.body);
-  // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newPortfolioItem.attr('category', this.category);
+  $newPortfolioItem.find('a').attr('href', this.repositoryUrl);
+  $newPortfolioItem.find('.readme').html(this.body);
 
-  $newArticle.append('<hr>');
+  $newPortfolioItem.find('time[pubdate]');
+
+  $newPortfolioItem.append('<hr>');
 
   //This cloned article is no longer a template, so we should remove that class...
-  $newArticle.removeClass('template');
-  return $newArticle;
+  $newPortfolioItem.removeClass('template');
+  return $newPortfolioItem;
 };
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
 
 rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
+  portfolioItems.push(new Portfolio(ele));
 });
 
-articles.forEach(function(a){
+portfolioItems.forEach(function(a){
   $('#articles').append(a.toHtml());
 });
