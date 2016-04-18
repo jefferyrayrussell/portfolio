2979cc3571d8a4aa2e4a080a1d48913d40060d3f
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-04-18 16:39:03 -0700

    commented out attempts at modullation of the constructor inorder to do class-06 assignment

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 //The code below is an object constructor which creates an object containing the essential properties for each of my projects.  The variable projectDataItems is an array that will store the various instances of the object.
 var projectDataItems = [];
-/*
+
 function ProjectItem (object){
   this.name = object.name;
   this.id = object.id;
@@ -11,48 +11,49 @@ function ProjectItem (object){
   this.location = object.location;
   this.summary = object.summary;
 };
-*/
-// Alternative to the above?
 
+// Alternative to the above?
+/*
 function ProjectItem (projectDataItems) {
   for (key in projectDatatItems) {
     this[key] = projectDataItems[key];
   };
 };
+*/
 
-// The code below is a methody using jQuery and Handlebars to organizes my coding projects so that they can later be appended to my web page.  The project-template is found on the index.html page.
-  /*  ProjectItem.prototype.toHtml = function() {
-      var $source = $('#project-template').html();
-      var template = Handlebars.compile($source);
-      return template(this);
-    };
+// The code below is a method using jQuery and Handlebars to organizes my coding projects so that they can later be appended to my web page.  The project-template is found on the index.html page.
+ProjectItem.prototype.toHtml = function() {
+  var $source = $('#project-template').html();
+  var template = Handlebars.compile($source);
+  return template(this);
+};
 
-    ProjectItem.prototype.filterNameToHtml = function() {
-      var template = Handlebars.compile($('#name-filter-template').html());
-      return template(this);
-    };
-  */
+ProjectItem.prototype.filterNameToHtml = function() {
+  var template = Handlebars.compile($('#name-filter-template').html());
+  return template(this);
+};
+
 // Alternative to the above?
-
+/*
 ProjectItem.prototype.toHtml = function(template) {
   var template = Handlebars.compile($('template').html());
   return template(this);
 };
-
+*/
 
 // The code below takes the projectData from each of the my projects found on the projects.js file and pushes it through the projectItem constructor above and then stores it in an array called projectDataItems which was introduced above.
 projectData.forEach(function(ele) {
   projectDataItems.push(new ProjectItem(ele));
 });
 
-  // The code below does the actual appending to the web page. It is appended at the class location identified as portfolio-projects.
-  projectDataItems.forEach(function(a){
-    $('#portfolio-projects').append(a.toHtml());
-    $('#name-filter').append(a.filterNameToHtml());
-  });
+// The code below does the actual appending to the web page. It is appended at the class location identified as portfolio-projects.
+projectDataItems.forEach(function(a){
+  $('#portfolio-projects').append(a.toHtml());
+  $('#name-filter').append(a.filterNameToHtml());
+});
 
 // Alternative to the above?
-
+/*
 projectDataItems.forEach(function(a) {
   $('#portfolio-projects').append(a.toHtml('#project-template'));
   if (categories.indexOf(a.category)){
@@ -60,3 +61,4 @@ projectDataItems.forEach(function(a) {
     categories.push(a.category);
   };
 });
+*/
