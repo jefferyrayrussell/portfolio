169edb7d99decf7477d0bc5edff2434994908ac0
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-04-27 08:58:16 -0700

    added repo.js file and moved old data file into data

---
+++ scripts/model/repo.js
@@ -0,0 +1,27 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url:     'https://api.github.com/users/jefferyrayrussell/repos' +
+               '?per_page=10' +
+               '&sort=updated',
+      type:    'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        repos.all = data;
+        callback();
+      }
+    });
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
